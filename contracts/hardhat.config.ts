e9a2edf43bda69a708c20e92606073418cbc0090
qpzm <lhm1442@gmail.com>
2023-05-19 22:07:15 +0900

    [ZKR-001] deploy: ERC20Verifier in mumbai

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,25 +1,12 @@
-import {task} from "hardhat/config";
+/** @type import('hardhat/config').HardhatUserConfig */
+import * as dotenv from "dotenv"
+import "@nomiclabs/hardhat-ethers";
+import "hardhat-deploy"
+// import "@nomicfoundation/hardhat-toolbox"
+// import "./tasks";
 
-import("@nomiclabs/hardhat-waffle");
-import("hardhat-gas-reporter");
-require("dotenv").config();
+dotenv.config()
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 const config = {
   solidity: {
     version: "0.8.18",
@@ -29,6 +16,11 @@ const config = {
         runs: 200,
       },
     },
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    }
   },
   networks: {
     sepolia: {
