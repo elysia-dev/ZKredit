a2b3cae4be16090826f947332b1b533abaaf7a36
qpzm <lhm1442@gmail.com>
2023-05-20 17:13:27 +0900

    [ZKR-006] feat: check if creditScore > 100

---
+++ contracts/schemas/json/request.js
@@ -0,0 +1,37 @@
+// update with your contract address
+const deployedContractAddress = "0x60a3dB603E4a3c141773237533750bb6390BDb42";
+
+// more info on query based requests: https://0xpolygonid.github.io/tutorials/wallet/proof-generation/types-of-auth-requests-and-proofs/#query-based-request
+// qrValueProofRequestExample: https://github.com/0xPolygonID/tutorial-examples/blob/main/on-chain-verification/qrValueProofRequestExample.json
+const qrProofRequestJson = {
+  id: "7f38a193-0918-4a48-9fac-36adfdb8b542",
+  typ: "application/iden3comm-plain-json",
+  type: "https://iden3-communication.io/proofs/1.0/contract-invoke-request",
+  thid: "7f38a193-0918-4a48-9fac-36adfdb8b542",
+  body: {
+    reason: "airdrop participation",
+    transaction_data: {
+      contract_address: deployedContractAddress,
+      method_id: "b68967e2",
+      chain_id: 80001,
+      network: "polygon-mumbai"
+    },
+    scope: [
+      {
+        id: 1,
+        circuitId: "credentialAtomicQuerySigV2OnChain",
+        query: {
+          allowedIssuers: ["*"],
+          context:
+            "https://raw.githubusercontent.com/elysia-dev/ZKredit/main/contracts/schemas/json-ld/kyc-v2.jsonld",
+          credentialSubject: {
+            creditScore: {
+              $gt: 100
+            }
+          },
+          type: "KYCPersonalCreditCredential"
+        }
+      }
+    ]
+  }
+};
--- contracts/tasks/index.ts
+++ contracts/tasks/index.ts
@@ -1,1 +1,2 @@
 export { setRequestTask } from "./setRequest"
+export { setRequestERC721Task } from "./setRequestERC721"
