373346a40ffe04a003887d232b4e6e6967e047dd
donguks <donguk.seo@modori.io>
2023-05-20 21:04:25 +0900

    [ZKR-11] Add contents in main

---
+++ ui/pages/index.jsx
@@ -0,0 +1,29 @@
+export default function Home() {
+  return (
+    <div>
+      <div className="flex justify-center items-center">
+        <span className="mt-40 mb-10 text-3xl font-bold text-transparent bg-clip-text bg-blue-500">
+          Zero Knowledge Credit Lending Protocol
+        </span>
+      </div>
+      <div className="flex justify-center items-center">
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+          <div href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <p class="font-normal text-gray-700 dark:text-gray-400">Utilizing Polygon ID</p>
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Trusted Personal Credit Lending</h5>
+          </div>
+          <div href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <p class="font-normal text-gray-700 dark:text-gray-400">Breaking away from uniform rates</p>
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Offering diverse criteria and interest rates</h5>
+          </div>
+          <div href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <p class="font-normal text-gray-700 dark:text-gray-400">In Web3.0</p>
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Providing a new investment opportunity</h5>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center items-center">
+      </div>
+    </div>
+  );
+}
